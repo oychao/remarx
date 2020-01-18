e0d1c99f2090a2b7a6d9894b2378f76285127fb2
ouyangchao <chao@ouyang.io>
2020-01-18 13:39:12 +0800

    feature: better config

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,24 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as parser from '@typescript-eslint/typescript-estree';
 
-import { parseConfig } from './configParser';
+import { config } from './config';
 
 export async function main() {
-  const config = await parseConfig();
   try {
-    const enterPath = path.resolve(config.root, config.source.dir, config.source.index);
+    const enterPath = path.resolve(config.root, config.main.sourceFolder, config.main.entranceFile);
     const enterFileBuffer = await fs.promises.readFile(enterPath);
     const enterFileStr = enterFileBuffer.toString();
 
     const astStr = JSON.stringify(parser.parse(enterFileStr), null, 2);
 
-    if (config.debug.ast) {
-      const astFolder = path.resolve(config.root, config.debug.output);
+    if (config.debug?.astDir) {
+      const astFolderPath = path.resolve(config.root, config.debug.astDir);
       try {
-        await fs.promises.access(astFolder);
+        await fs.promises.access(astFolderPath);
       } catch (error) {
-        fs.promises.mkdir(astFolder);
+        fs.promises.mkdir(astFolderPath);
       }
-      const astDir = path.resolve(astFolder, 'index.json');
+      const astDir = path.resolve(astFolderPath, 'index.json');
       await fs.promises.writeFile(astDir, astStr);
     }
 
