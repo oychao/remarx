c401ca34150de9bcc5ad4b3f08b52aa4a94f5bb5
ouyangchao <chao@ouyang.io>
2020-01-26 19:35:11 +0800

    chore: remove unnecessary interfaces

--- src/utils.ts
+++ src/utils.ts
@@ -36,35 +36,9 @@ export async function outputType(ast: object): Promise<void> {
     path.resolve(__projectRoot, 'src', 'parser', 'node', 'astTypes.ts'),
     `import { ConcreteNode } from './astNode';
 
-export enum AstType {\n${types.map(type => `  ${type} = '${type}'`).join(',\n')},\n}
-
-${types
-  .map(
-    type => `export interface Node${type}Visitable {
-  visit${type}(path: ConcreteNode[], node: ConcreteNode, parent: ConcreteNode, grantParent: ConcreteNode): Promise<void>;
-}`
-  )
-  .join('\n\n')}
-`
+export enum AstType {\n${types.map(type => `  ${type} = '${type}'`).join(',\n')},\n}\n`
   );
 }
-
-// export async function fileExists(filePath: string): Promise<boolean> {
-//   return new Promise(resolve => {
-//     fs.access(filePath, fs.constants.F_OK, err => {
-//       if (err) {
-//         resolve(false);
-//       }
-//       fs.lstat(filePath, (err, stats) => {
-//         if (err || !stats.isFile()) {
-//           resolve(false);
-//         } else {
-//           resolve(true);
-//         }
-//       });
-//     });
-//   });
-// }
 
 export async function fileExists(filePath: string): Promise<boolean> {
   try {
