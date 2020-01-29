6d6f506ae7b3ffe19d3c14106218532794dfa40d
oychao <chao@ouyang.io>
2020-01-29 14:52:49 +0800

    feature: delete useless code

--- src/parser/node/baseNode.ts
+++ src/parser/node/baseNode.ts
@@ -1,20 +1,12 @@
 import { BaseNode, SourceLocation, Range } from '@typescript-eslint/typescript-estree/dist/ts-estree/ts-estree';
 
 export class ConcreteBaseNode implements BaseNode {
-  public loc: SourceLocation = {
-    start: {
-      line: -1,
-      column: -1,
-    },
-    end: {
-      line: -1,
-      column: -1,
-    },
-  };
-  public range: Range = [-1, -1];
+  public loc: SourceLocation;
+  public range: Range;
 
   constructor(astNode: BaseNode) {
     this.loc = astNode.loc;
     this.range = astNode.range;
   }
 }
+ 
