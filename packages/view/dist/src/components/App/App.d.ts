5fc802f4262b33b140d687a229c6d77118c8fa02
oychao <chao@ouyang.io>
2020-02-22 15:24:29 +0800

    chore: update README, remove build files

--- packages/view/dist/src/components/App/App.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import './style.less';
-export declare function App(): JSX.Element;
--- packages/view/dist/src/components/DepGraph/DepGraph.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { NodeStyle } from './DepNode';
-interface DepGraphProps {
-    graphModel: GraphView;
-    determineStyle?: (node: dagre.Node) => NodeStyle;
-    onNodeClick?: (path: string) => void;
-}
-export declare function DepGraph({ graphModel, determineStyle, onNodeClick }: DepGraphProps): JSX.Element;
-export {};
